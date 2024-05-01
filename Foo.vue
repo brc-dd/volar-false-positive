b8fa10a62437e679add23f8f115bb16546cb2f69
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-05-02 02:27:21 +0530

    init

---
+++ Foo.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+function onItemInteraction(e: MouseEvent | Event) {}
+</script>
+
+<template>
+  <div v-on="{ click: onItemInteraction, keydown: onItemInteraction }"></div>
+</template>
